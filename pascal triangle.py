92c017aa4b399bd61e54704976853a5145ff986a
tattooday <38026296+tattooday@users.noreply.github.com>
2018-04-03 08:00:14 +0000

    Update pascal triangle.py

--- pascal triangle.py
+++ pascal triangle.py
@@ -26,3 +26,5 @@ def printit(n):
     return
         
 printit(10)
+
+#    max2, max1, fill = 3,4, 1  #Change maximum capacity and final capacity    print("%d\t%d" % (jug1, jug2))    if jug2 == fill:        return    elif jug2 == max2:        pour(0, jug1)    elif jug1 != 0 and jug2 == 0:        pour(0, jug1)    elif jug1 == fill:        pour(jug1, 0)    elif jug1 < max1:        pour(max1, jug2)    elif jug1 < (max2-jug2):        pour(0, (jug1+jug2))    else:        pour(jug1-(max2-jug2), (max2-jug2)+jug2)

bf6b1c1479a339475701407b829b734b298ef89f
T.M <38026296+tattooday@users.noreply.github.com>
2018-05-09 12:30:58 +0100

    Update edit distance.py

--- edit distance.py
+++ edit distance.py
@@ -21,7 +21,7 @@ def edit(a,b):
         
     for l in range(1,len_a+1):
         for m in range(len_b+1):
-            if len_a==len_b:
+            if a[l-1]==b[m-1]:
                 c[l][m]=min(c[l-1][m]+1,c[l][m-1]+1,c[l-1][m-1])
             else:
                 c[l][m]=min(c[l-1][m]+1,c[l][m-1]+1,c[l-1][m-1]+1)

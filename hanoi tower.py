a4afe1b67e2c9210c769952c38f088a58fe542ee
T.M <38026296+je-suis-tm@users.noreply.github.com>
2020-06-09 19:49:36 +0100

    restructure

--- hanoi tower.py
+++ hanoi tower.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 #hanoi tower is the classic recursion
@@ -9,9 +9,14 @@
 #rules can be seen from this website:
 # https://en.wikipedia.org/wiki/Tower_of_Hanoi
 
+
+# In[2]:
+
+
 def hanoi(n,a,b,c):
+    
     #rule states that each time we can only move one element
-    #so when the recusion reaches to base case 1
+    #so when the recursion reaches to base case 1
     #we print the movement of elements from a to c
     if n==1:
         print(a,'-',c)
@@ -22,13 +27,21 @@ def hanoi(n,a,b,c):
     #note that we set print a to c when n reaches one
     #so in this case we reorder the function, replace c with column b where elements actually move towards
     hanoi(n-1,a,c,b)
+    
     #the second step is to move the base case from column a to column c
     #we are only moving base case, thats why n=1
     hanoi(1,a,b,c)
+    
     #final step would be move everything above base case from column b to column c
     hanoi(n-1,b,a,c)
     
+    
+# In[3]:
+
+
 hanoi(4,'a','b','c')
-# the best explanation i have discovered should be
+
+
+# the best explanation should be
 # https://www.python-course.eu/towers_of_hanoi.php
 
